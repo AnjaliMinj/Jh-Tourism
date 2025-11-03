6c6e84286b3f10aad1f1dc6a07ddebc92537aea3
AnjaliMinj <aminj5112@gmail.com>
2025-11-03 19:17:01 +0530

    Initial commit

---
+++ frontend/script.js
@@ -0,0 +1,15 @@
+// Initialize AOS animations
+AOS.init({
+  duration: 1000,
+  once: true
+});
+
+// Smooth scroll for internal links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
